48dcdfcbe7cda0cc277e8558361596ab830f48c7
DaltonBailey <drbailey117@gmail.com>
2024-01-20 04:54:30 -0600

    Ronald

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,6 +4,8 @@ import LoginPage from "./loginpage/LoginPage";
 import Lobby from "./lobby/Lobby";
 import FeedPage from "./feed/Feed";
 import ProfilePage from "./profilepage/ProfilePage";
+import PostGame from "./postgame/PostGame";
+
 import "./App.css";
 
 function App() {
@@ -12,8 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/lobby" element={<Lobby />} />
+        <Route path="/pregame" element={<PostGame />} />
         <Route path="/timeline" element={<FeedPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/postgame" element={<PostGame />} />
       </Routes>
     </Router>
   );
